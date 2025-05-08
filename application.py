d5427568a2e67a09963b7bf27e91e7f2e1298ca6
Dr. C. F. Roberts <complliance@wealthbt.com>
2025-05-07 20:28:27 -0400

    Enable CORS to allow frontend access

--- application.py
+++ application.py
@@ -1,4 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+from flask import Flask, request, jsonify
 import requests
 import json
 
